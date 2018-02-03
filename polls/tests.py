3d4e381c949d7fecb1f549aa3e19e0fa602980c1
gord chung <gord@live.ca>
2018-02-03 17:06:05 -0500

    add form support
    - save user response
    - allow only one response per day

--- polls/tests.py
+++ polls/tests.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+import mock
+
 from django import test
-from django.contrib.auth import models
+from django.contrib.auth import models as auth_models
+
+from . import models
 
 
 class ViewTests(test.TestCase):
 
     def setUp(self):
-        self.group1 = models.Group(name='group1')
+        self.group1 = auth_models.Group(name='group1')
         self.group1.save()
-        self.user1 = models.User.objects.create_user(username='user1')
+        self.user1 = auth_models.User.objects.create_user(username='user1')
         self.user1.groups.add(self.group1)
         self.user1.save()
-        self.group2 = models.Group(name='group2')
+        self.group2 = auth_models.Group(name='group2')
         self.group2.save()
-        self.user2 = models.User.objects.create_user(username='user2')
+        self.user2 = auth_models.User.objects.create_user(username='user2')
         self.user2.groups.add(self.group2)
         self.user2.save()
-        self.user3 = models.User.objects.create_user(username='user3')
+        self.user3 = auth_models.User.objects.create_user(username='user3')
         self.user3.groups.add(self.group2)
         self.user3.save()
 
@@ -65,3 +70,51 @@ class ViewTests(test.TestCase):
         self.assertRedirects(response, '/polls/login',
                              status_code=302, target_status_code=301)
         self.assertNotIn('_auth_user_id', self.client.session)
+
+    def test_user_one_daily_response(self):
+        self.client.force_login(self.user1)
+        response = self.client.get('/polls/')
+        self.assertEqual(200, response.status_code)
+        models.Choice.objects.create(value=5, user=self.user1)
+        response = self.client.get('/polls/')
+        self.assertRedirects(response, '/polls/results',
+                             status_code=302, target_status_code=301)
+
+    def test_user_one_daily_response_logout(self):
+        self.client.force_login(self.user1)
+        response = self.client.get('/polls/')
+        self.assertEqual(200, response.status_code)
+        models.Choice.objects.create(value=5, user=self.user1)
+        self.client.logout()
+        self.client.force_login(self.user1)
+        response = self.client.get('/polls/')
+        self.assertRedirects(response, '/polls/results',
+                             status_code=302, target_status_code=301)
+
+    def test_user_response_isolated(self):
+        self.client.force_login(self.user1)
+        response = self.client.get('/polls/')
+        self.assertEqual(200, response.status_code)
+        models.Choice.objects.create(value=5, user=self.user1)
+        self.client.logout()
+        self.client.force_login(self.user2)
+        response = self.client.get('/polls/')
+        self.assertEqual(200, response.status_code)
+
+    def test_user_next_day_fine(self):
+        self.client.force_login(self.user1)
+        response = self.client.get('/polls/')
+        self.assertEqual(200, response.status_code)
+        prev_day = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+        with mock.patch('django.utils.timezone.now', return_value=prev_day):
+            models.Choice.objects.create(timestamp=prev_day.date(), value=5,
+                                         user=self.user1)
+        response = self.client.get('/polls/')
+        self.assertEqual(200, response.status_code)
+
+    def test_user_no_response_no_redirect(self):
+        self.client.force_login(self.user1)
+        response = self.client.get('/polls/')
+        self.assertEqual(200, response.status_code)
+        response = self.client.get('/polls/')
+        self.assertEqual(200, response.status_code)
--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+
+from django import shortcuts as sc
 from django.views import generic
 from django.views.generic import edit
 
-from . import forms
+from . import forms, models
 
 
 class VoteView(edit.FormView):
     form_class = forms.ChoiceForm
     template_name = 'polls/vote.html'
     success_url = '/polls/results'
+
+    def get(self, request, *args, **kwargs):
+        now = datetime.datetime.utcnow()
+        tomorrow = now + datetime.timedelta(days=1)
+        if models.Choice.objects.filter(timestamp__gte=now.date(),
+                                        timestamp__lt=tomorrow.date(),
+                                        user=request.user).exists():
+            return sc.redirect('/polls/results')
+        return super(VoteView, self).get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        resp_obj = models.Choice(value=int(form.cleaned_data['choice_field']),
+                                 user=self.request.user)
+        resp_obj.save()
+        return super(VoteView, self).form_valid(form)
 
 
 class ResultsView(generic.TemplateView):
